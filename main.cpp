473f81b2d8b47f12acd903a0b99006e20617bf87
Nedra Seid <nedraseid@gmail.com>
2026-06-17 09:42:13 +0300

    nested id in c++

--- main.cpp
+++ main.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int score = 60;
-    if(score >= 95){
-        cout << "fantastic kid" << endl;
-    }
-    else if (score >=85){
-        cout << "grate kid" << endl;
-    }
-    else if(score >=75){
-        cout << "good kid" << endl;
-    }
-    else if(score >=65){
-        cout << "normal kid" << endl;
+    bool isSunny = false;
+    bool isWeekend = false;
+    
+    if (isSunny){
+        if(isWeekend){
+            cout << "I will be happy and go to my mom's house" << endl;
+        }
+        else{
+            cout << "I will be happy but not go to my mom's house" << endl;
+        }
     }
     else{
-        cout << "you need to study buddy" << endl;
+        if(isWeekend){
+            cout << "Iam not happy but I will go to my mom's house atleast" << endl;
+        }
+        else{
+            cout << "I am not gonna be happy and i am not going to my moms house" << endl;
+        }
+
     }
     return 0;
 }
